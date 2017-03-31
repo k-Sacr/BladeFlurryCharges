71d1d3c2baf8f21d1fdec8b5ab9ccb229e10d233
k-Sacr <kg.sacred@gmail.com>
2017-03-31 20:39:07 +0700

    Add project

---
+++ src/BladeFlurryCharges/BladeFlurryCharges.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.RemoteMemoryObjects;
+using static BladeFlurryCharges.WinApiMouse;
+
+namespace BladeFlurryCharges
+{
+    public class BladeFlurryCharges : BaseSettingsPlugin<BladeFlurryChargesSetting>
+    {
+        public override void Render()
+        {
+            var buffs = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>().Buffs;
+
+            if(buffs.Exists(b => b.Name == "charged_attack" && b.Charges==6))
+                MouseTools.MouseRightDown();
+
+            //foreach (Buff buff in playerHealth.Buffs)
+            //    {
+            //        if (buff.Name == "charged_attack")
+            //            if (buff.Charges == 6)
+            //    }
+        }
+    }
+
+    internal static class MouseTools
+    {
+       public static void MouseRightDown()
+        {
+            MouseEvent(MouseEventFlags.RightUp);
+            Thread.Sleep(70);
+            MouseEvent(MouseEventFlags.RightDown);
+        }
+
+        private static Point GetCursorPosition()
+        {
+            Point currentMousePoint;
+            return GetCursorPos(out currentMousePoint) ? new Point(currentMousePoint.X, currentMousePoint.Y) : new Point(0, 0);
+        }
+
+        private static void MouseEvent(MouseEventFlags value)
+        {
+            var position = GetCursorPosition();
+
+            mouse_event
+                ((int)value,
+                    position.X,
+                    position.Y,
+                    0,
+                    0)
+                ;
+        }
+    }
+
+
+    public class BladeFlurryChargesSetting : SettingsBase
+    {
+        public BladeFlurryChargesSetting()
+        {
+            Enable = true;
+        }
+    }
+
+
+    public static class WinApiMouse
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out Point lpMousePoint);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        
+        #region Structs/Enums
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            
+        }
+
+        #endregion
+    }
+}
+
+
+
+
+
