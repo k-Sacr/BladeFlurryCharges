441a8b3f6cd896556ed0d8597365fbf9c89913a6
Detective Squirrel <ratman7539@windowslive.com>
2017-08-28 05:02:51 +1200

    Final Update

--- src/BladeFlurryCharges/BladeFlurryChargesSettings.cs
+++ src/BladeFlurryCharges/BladeFlurryChargesSettings.cs
@@ -1,10 +1,5 @@
 ﻿using PoeHUD.Hud.Settings;
 using PoeHUD.Plugins;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BladeFlurryCharges
 {
@@ -13,14 +8,9 @@ namespace BladeFlurryCharges
         public BladeFlurryChargesSettings()
         {
             LeftClick = false;
-            ReleaseRightClick = false;
         }
-
-
+        
         [Menu("Use Left Click", 1)]
         public ToggleNode LeftClick { get; set; }
-
-        [Menu("Release Right Click", 132, 1)]
-        public ToggleNode ReleaseRightClick { get; set; }
     }
 }
